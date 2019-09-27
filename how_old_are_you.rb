7ffe9f7c983450e924564271729471643b314e9b
Melissa Hummelt <mkhummelt@gmail.com>
2019-09-27 18:25:45 +0000

    Automatically backed up by Learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -1,4 +1,5 @@
 require_relative './current_age_for_birth_year.rb'
+<<<<<<< HEAD
 
 puts "What year were you born?"
 birth_year = gets.to_i
@@ -6,3 +7,12 @@ birth_year = gets.to_i
 users_age = current_age_for_birth_year(birth_year) + (Time.now.year - 2003)
 
 puts "You are: " + users_age.to_s + " years old."
+=======
+ 
+puts "What year were you born?"
+birth_year = gets.to_i
+ 
+users_age = current_age_for_birth_year(birth_year) + (Time.now.year - 2003)
+ 
+puts "You are: " + users_age.to_s + " years old."
+>>>>>>> bd96c9ccbe7b14c87cb5b6b2695fbeedb07bf537
